039b6cb1adc0f338d40213308bc79dc37bcceebd
Daveee10 <daver16@msn.com>
2012-07-24 21:02:25 +0200

    Change Cyanogen vendor to JB vendor and change device specific version string

--- cm.mk
+++ cm.mk
@@ -1,8 +1,8 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common -JBMiniProject and CM stuff.
+$(call inherit-product, vendor/jbmp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/semc/shakira/shakira.mk)
@@ -16,7 +16,7 @@ PRODUCT_MODEL := X8
 PRODUCT_MANUFACTURER := Sony Ericsson
 
 # Release data
-PRODUCT_VERSION_DEVICE_SPECIFIC := 01
+PRODUCT_VERSION_DEVICE_SPECIFIC := 1
 TARGET_NO_LIVEWALLPAPERS := false
 TARGET_BOOTANIMATION_NAME := vertical-320x480
 
