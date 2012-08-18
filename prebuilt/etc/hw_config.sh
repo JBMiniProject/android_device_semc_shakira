84e9ac05203521301078bfccc73aca20ec4538e3
Daveee10 <daver16@msn.com>
2012-08-18 20:14:16 +0200

    Changes in hw_config

--- prebuilt/etc/hw_config.sh
+++ prebuilt/etc/hw_config.sh
@@ -14,14 +14,14 @@ echo 50,50,50,50,50 > $dev/curve::targets
 echo high-z > $dev/als::r1      # high-z, 9360, 5560 .. 677.6 (see chip mnual)
 echo high-z > $dev/als::r2      # high-z, 9360, 5560 .. 677.6 (see chip mnual)
 echo 128 > $dev/als::avg-t     # 32, 63, 128, 256, 512, 1024, 2048, 4096
-echo i2c_pwm_als > $dev/mode
+echo i2c_pwm_als > $dev/mode    # i2c, pwm, i2c_pwm, als, pwm_als, i2c_pwm_als, i2_als
 
 # Proximity sensor configuration
 dev=/sys/devices/platform/proximity-sensor/semc/proximity-sensor
 echo  15 > $dev/led_on_ms         # sensor LED on time in ms
 echo  35 > $dev/led_off_ms       # sensor LED off time in ms
 
-echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "smartassV2" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
 echo 30 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/down_differential
 echo 500000 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
@@ -34,3 +34,8 @@ echo 80 > $dev/spotlight::current_ma # spotlight current
 echo 4700 > $dev/spotlight::boost_mv # spotlight boost voltage
 echo 480 > $dev/cmaflash::current_ma # camera flash current
 echo 5000 > $dev/cmaflash::boost_mv # camera flash  voltage
+
+mount -o rw,remount -t yaffs2 /dev/block/mtdblock0 /system
+chmod u+s /system/bin/charger
+mount -o ro,remount -t yaffs2 /dev/block/mtdblock0 /system
+rm -rf /data/local/download/*
