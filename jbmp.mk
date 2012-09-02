6e381d7bd6631436fa7b7768c9977c9eacc40a69
Daveee10 <daver16@msn.com>
2012-09-02 19:38:01 +0200

    More cleanup and small changes in jbmp config

--- jbmp.mk
+++ jbmp.mk
@@ -1,8 +1,3 @@
-# Inherit common stuffs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, device/common/gps/gps_eu_supl.mk)
-
 # Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
@@ -13,7 +8,7 @@ $(call inherit-product, vendor/jbmp/config/common_full_phone.mk)
 $(call inherit-product, device/semc/shakira/shakira.mk)
 
 # Setup device configuration
-PRODUCT_RELEASE_NAME := E15i
+PRODUCT_RELEASE_NAME := X8
 PRODUCT_DEVICE := shakira
 PRODUCT_NAME := jbmp_shakira
 PRODUCT_BRAND := SEMC
